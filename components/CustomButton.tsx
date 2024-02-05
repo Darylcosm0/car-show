7fc3c2ee626f09f98954a8f96dbc55977c97445d
Darylcosm0 <mutaidaryl@gmail.com>
2024-02-05 14:47:34 +0300

    :tada:finished navbar

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,11 +2,11 @@
 import Image from "next/image";
 import { CustomButtonProps } from "@/types";
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
   return (
     <button
     disabled={false}
-    type="button"
+    type={btnType || "button"}
     className={`bg-primary-blue text-white rounded-full mt-10 ${containerStyles}`}
     onClick={handleClick}
 >

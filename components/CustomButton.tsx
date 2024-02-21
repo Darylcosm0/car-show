e1693e241e1f6a9777e9513a2b48cc408eee035c
Darylcosm0 <mutaidaryl@gmail.com>
2024-02-21 15:20:02 +0300

    updated button

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,17 +2,34 @@
 import Image from "next/image";
 import { CustomButtonProps } from "@/types";
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+}: CustomButtonProps) => {
   return (
     <button
-    disabled={false}
-    type={btnType || "button"}
-    className={`bg-primary-blue text-white rounded-full mt-10 ${containerStyles}`}
-    onClick={handleClick}
->
-  <span className="flex-1">{title}</span>
-</button>
-
-)};
+      disabled={false}
+      type={btnType || "button"}
+      className={`bg-primary-blue text-white rounded-full mt-10 ${containerStyles}`}
+      onClick={handleClick}
+    >
+        <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
+  </button>
+  );
+};
 
 export default CustomButton;

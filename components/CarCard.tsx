dc72a67d96e236b53f05b3d9f67b776a1e255e29
Darylcosm0 <mutaidaryl@gmail.com>
2024-02-22 12:19:59 +0300

    completed carcard

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,9 +2,10 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import { calculateCarRent , generateCarImageUrl} from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import { CarProps } from "@types";
 import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -78,8 +79,11 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
-
-     
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
